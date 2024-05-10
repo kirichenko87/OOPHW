4680d15602c08db76b673628343b638b4c174378
kirichenko87 <kappa134rus@gmail.com>
2024-05-11 00:03:17 +0300

    first commit

---
+++ main_1-3.py
@@ -0,0 +1,25 @@
+class PassengerPlane:
+
+
+	def __init__(self, manufacturer: str , airplane: str, sit_place: int, flight_height: int, speed_airplane: int):
+		self.manufacturer = manufacturer
+		self.airplane = airplane
+		self.sit_place = sit_place
+		self.flight_height = flight_height
+		self.speed_airplane = speed_airplane
+
+	def airplane_down(self):
+		pass
+
+	def airplane_up(self):
+		pass
+
+	def change_height(self):
+		pass
+
+	def change_speed(self):
+		pass
+
+	def get_info(self):
+		pass
+
