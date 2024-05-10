cf64955fa8d0884ea29ad4438986e5a3554ffc58
kirichenko87 <kappa134rus@gmail.com>
2024-05-10 09:12:14 +0300

    first commit

---
+++ main_1.py
@@ -0,0 +1,10 @@
+class Animal:
+	def __init__(self, name_animal, type_animal, age_animal):
+		self.name_animal = name_animal
+		self.type_animal = type_animal
+		self.age_animal = age_animal
+
+	def sound_animal(self):
+		pass
+	def getinfo(self):
+		pass
