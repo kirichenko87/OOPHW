4a1200d0c1d8289ff09c7aae7a50f14b5b625127
kirichenko87 <kappa134rus@gmail.com>
2024-05-10 22:09:18 +0300

    first commot

---
+++ main_1-2.py
@@ -0,0 +1,17 @@
+class Book:
+	def __init__(self, title_book: str, autor_book: str, number_pages: str):
+		self.title_book = title_book
+		self.autor_book = autor_book
+		self.number_pages = number_pages
+
+	def open_book(self):
+		pass
+
+	def info_book(self):
+		pass
+
+	def get_info(self):
+		pass
+
+
+	
