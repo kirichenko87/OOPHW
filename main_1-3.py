e184d495efe1f66a8bcbd103b8a11db09bcc7d40
kirichenko87 <kappa134rus@gmail.com>
2024-05-11 07:11:53 +0300

    make "get_info"

--- main_1-3.py
+++ main_1-3.py
@@ -1,7 +1,6 @@
 class PassengerPlane:
 
-
-	def __init__(self, manufacturer: str , airplane: str, sit_place: int, flight_height: int, speed_airplane: int):
+	def __init__(self, manufacturer: str, airplane: str, sit_place: int, flight_height: int, speed_airplane: int):
 		self.manufacturer = manufacturer
 		self.airplane = airplane
 		self.sit_place = sit_place
@@ -9,17 +8,32 @@ class PassengerPlane:
 		self.speed_airplane = speed_airplane
 
 	def airplane_down(self):
-		pass
+		if self.flight_height == 0 and self.speed_airplane == 0:
+			print('Самолет приземлился')
 
 	def airplane_up(self):
-		pass
+		if self.flight_height > 500:
+			print('Самолет взлетел')
 
-	def change_height(self):
-		pass
+	def change_height(self, new_height: int):
+		self.flight_height = new_height
 
-	def change_speed(self):
-		pass
+	def change_speed(self, new_speed: int):
+		self.speed_airplane = new_speed
 
 	def get_info(self):
-		pass
+		if self.flight_height < 1:
+			print(f'Уважаемые дамы и господа, мы рады вас приветствовать на борту самолеты {self.airplane},'
+				  f' произведеный компание {self.manufacturer}, на борту полная посадка {self.sit_place} человек, скоро мы взлетим')
+		else:
+			print(f'Уважаемые дамы и господа, мы рады вас приветствовать на борту самолеты {self.airplane},'
+				  f' произведеный компание {self.manufacturer}, на борту полная посадка {self.sit_place} человек, скоро находимся на высоте'
+				  f'{self.flight_height} скорость полета {self.speed_airplane}')
 
+
+class Program:
+
+
+	@staticmethod
+	def main():
+		pass
