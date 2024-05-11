fe316dea0e4ecfbb1776e3cf48402482a6f9b5f4
kirichenko87 <kappa134rus@gmail.com>
2024-05-11 08:16:26 +0300

    add "add_track, delete_track, play_track"

--- main_1-4.py
+++ main_1-4.py
@@ -5,14 +5,21 @@ class MusicAlbum:
 		self.track_list = track_list
 
 
-	def add_track(self):
-		pass
+	def add_track(self, track: str):
+		self.track_list.append(track)
 
-	def delete_track(self):
-		pass
+	def delete_track(self, track: str):
+		if track in self.track_list:
+			self.track_list.remove(track)
+			print(f'{track} был удален из списка проигрываемых')
+		else:
+			print(f'{track} не найден возможно вы его удалили или не добавили ранее')
 
-	def play_track(self):
-		pass
+	def play_track(self, track: str):
+		if track in self.track_list:
+			print(f'Сейчас играет {track}')
+		else:
+			print('Трэк не найден')
 
 	def get_info_album(self):
 		pass
