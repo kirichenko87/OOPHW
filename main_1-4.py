3931d123f9c1c4a3efda2145bfbbcd7248f5933d
kirichenko87 <kappa134rus@gmail.com>
2024-05-11 08:03:05 +0300

    make "pattern"

---
+++ main_1-4.py
@@ -0,0 +1,22 @@
+class MusicAlbum:
+	def __init__(self, artist: str, name_album: str, track_list: list):
+		self.artist = artist
+		self.name_album = name_album
+		self.track_list = track_list
+
+
+	def add_track(self):
+		pass
+
+	def delete_track(self):
+		pass
+
+	def play_track(self):
+		pass
+
+	def get_info_album(self):
+		pass
+class Program:
+	@staticmethod
+	def main():
+		pass
